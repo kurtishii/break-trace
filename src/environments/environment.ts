dda8a37a6eeb46327c191a2ef2f493831abb823c
arben <arbenkurtishi@gmail.com>
2026-06-28 21:29:42 +0200

    config: add Supabase credentials

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  supabaseUrl: '',
-  supabaseAnonKey: '',
+  supabaseUrl: 'https://umiyefxfeajcydxeomch.supabase.co',
+  supabaseAnonKey: 'sb_publishable_Ol1crEmulIA1_dc0VPZUxQ_TiY2tVr8',
 };
